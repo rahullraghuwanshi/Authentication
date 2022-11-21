0d89b695db64012380ddb033f225f3e3d0f887a8
Rahul Raghuwanshi <champrahul202002@gmail.com>
2022-11-21 12:45:44 +0530

    presentation layer implemented

--- app/src/main/java/com/rahulraghuwanshi/assignment/presentation/BaseActivity.kt
+++ app/src/main/java/com/rahulraghuwanshi/assignment/presentation/BaseActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.rahulraghuwanshi.assignment.R
 
-class MainActivity : AppCompatActivity() {
+class BaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_base)
     }
 }
